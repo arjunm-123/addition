5cba5a2c72238f6e607c6de0625183d6d98e6cc2
HP <HP@DESKTOP-RJLER7D>
2023-06-14 20:24:32 +0530

    commit

---
+++ addition/src/addition/add.java
@@ -0,0 +1,17 @@
+package addition;
+
+public class add
+{
+
+	static int a=20;
+	static int b=35;
+	static int c=a+b;
+
+		public static void main (String args[])
+		{
+			
+			System.out.println(c);
+			
+	}
+	}
+
